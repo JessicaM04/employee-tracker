57bc73e72b3a788a9b492826f3e37f58d8bf5f38
JessicaM04 <jessica.mcnamara84@gmail.com>
2022-08-25 19:25:53 -0400

    added questions, routes to get/add info

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 const inquirer = require("inquirer");
-const mysql = require("mysql2/promise");
 
-// Setting up database
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "employee_tracker"
-  },
-  console.log("Connected to the employee-tracker database.")
-);
 // Setting up inquirer
-const questions = [
+ const questions = [
   {
     type: "list",
     name: "options",
@@ -21,35 +10,202 @@ const questions = [
   }
 ]
 
-inquirer
+const promptQuestions = (questions) => {
+  inquirer
   .prompt(questions)
   .then((answers) => {
-    switch(answers.options) {
-      case "View all departments":
-        getAllDepartments();
-      break;
-      case "Add a department":
-      break;
-      case "View all roles":
-      break;
-      case "Add a role":
-      break;
-      case "View all employees":
-      break;
-      case "Add an employee":
-      break;
-      case "Update an employee role":
-      break;
-      case "Quit":
-      break;
+    if(answers === "Quit") {
+      prompt.complete();
     }
-    console.log(answers);
+    userAnswerAction(answers);
+    // Handles user action (.then call prompt(questions))
   })
   .catch((error) => {
     console.log(error);
   });
+}
 
-  const getAllDepartments = (() => {
-    // Retrieve all dept from table
-    console.log("retrieving all departments from the table");
-  }); 
+const userAnswerAction = (answers) => {
+  switch(answers.options) {
+    case "View all departments":
+      getAllDepartments();
+      // promptQuestions(questions);
+    break;
+    case "Add a department":
+      addDepartment();
+    break;
+    case "View all roles":
+      getAllRoles();
+    break;
+    case "Add a role":
+      addRole();
+    break;
+    case "View all employees":
+      getAllEmployees();
+    break;
+    case "Add an employee":
+      addEmployee();
+    break;
+    case "Update an employee role":
+    break;
+    case "Quit":
+    break;
+  }
+}
+
+const getAllDepartments = (() => {
+  // Retrieve all departments from table
+  const mysql = require("mysql2");
+
+  // Setting up database
+  const db = mysql.createConnection(
+    {
+      host: "localhost",
+      user: "root",
+      password: "",
+      database: "employee_tracker"
+    },
+  );
+  db.query(
+    'SELECT * FROM department', 
+    function(err, results, fields) {
+      console.table(results);
+  });
+  promptQuestions(questions);
+
+});
+
+const getAllRoles = (() => {
+  // Retrieve all roles from table
+  const mysql = require("mysql2");
+
+  // Setting up database
+  const db = mysql.createConnection(
+    {
+      host: "localhost",
+      user: "root",
+      password: "",
+      database: "employee_tracker"
+    },
+  );
+  db.query(
+    'SELECT * FROM role', 
+    function(err, results, fields) {
+      console.table(results);
+  });
+  promptQuestions(questions);
+});
+
+const addRole = (() => {
+  const mysql = require("mysql2");
+  const db = mysql.createConnection(
+    {
+      host: "localhost",
+      user: "root",
+      password: "",
+      database: "employee_tracker"
+    },
+  );
+  inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "newRole",
+      message: "What is the new role you would like to add?",
+  }])
+  .then((answers) => {
+    console.log(answers);
+    db.query(
+      "INSERT INTO role (name) VALUES(?)",
+      [answers.newRole],
+      (err, result, fields) => {
+        console.log(answers);
+      }
+    );
+  });
+})
+
+const getAllEmployees = (() => {
+  // Retrieve all employees from table
+  const mysql = require("mysql2");
+
+  // Setting up database
+  const db = mysql.createConnection(
+    {
+      host: "localhost",
+      user: "root",
+      password: "",
+      database: "employee_tracker"
+    },
+  );
+  db.query(
+    'SELECT * FROM employee', 
+    function(err, results, fields) {
+      console.table(results);
+  });
+
+  promptQuestions(questions);
+});
+
+const addEmployee = (() => {
+  const mysql = require("mysql2");
+  const db = mysql.createConnection(
+    {
+      host: "localhost",
+      user: "root",
+      password: "",
+      database: "employee_tracker"
+    },
+  );
+  inquirer
+  .prompt([
+    {
+    type: "input",
+    name: "newEmployee",
+    message: "What is the name of the employee you would like to add?",
+  }])
+  .then((answers) => {
+    console.log(answers);
+    db.query(
+      "INSERT INTO employee (first_name, last_name) VALUES(?)",
+      [answers.newEmployee],
+      (err, result, fields) => {
+        console.log(answers);
+      }
+    );
+  });
+})
+
+const addDepartment = (() => {
+  const mysql = require("mysql2");
+
+  // Setting up database
+  const db = mysql.createConnection(
+    {
+      host: "localhost",
+      user: "root",
+      password: "",
+      database: "employee_tracker"
+    },
+  );
+  inquirer
+  .prompt([
+    {
+    type: "input",
+    name: "newDepartment",
+    message: "What is the name of the department you would like to add?",
+  }])
+  .then((answers) => {
+    console.log(answers);
+    db.query(
+      "INSERT INTO department (name) VALUES(?)",
+      [answers.newDepartment],
+      (err, result, fields) => {
+        console.log(results);
+      }
+    );
+  });
+})
+
+
+promptQuestions(questions);
